b54caf5118428f60b5b443a1e7e2634fc3abe59a
Irene Iruoma <ireneiruoma@gmail.com>
2025-08-17 21:36:58 +0100

    update on my calculator app

---
+++ Assignments/assignment1.py
@@ -0,0 +1,35 @@
+
+"""
+this is asimple calculator program that adds, subtracts,
+multuplies and divides
+
+num1 = 10
+num2 = 26
+
+"""
+
+num1 = int(input("Enter the first number; "))
+num2 = int(input("Enter the second number; "))
+operation = input(f"what mathematical operation do yo you want to perform? (addition, subtraction, multiplication,division): ")
+
+# sum = num1 + num2
+# subtraction = num1 - num2
+# multiplication = num1 * num2
+
+if operation == "addition":
+    print(f"This is the sum of the two numbers; {num1 + num2}")
+elif operation == "subtraction":
+    print(f"This is the difference of the two numbers; {num1 - num2}")
+elif operation == "multiplication":
+   print(f"This is the product of the two numbers; {num1 * num2}")
+elif operation == "division":
+  print(f"This is the quotient of the two numbers; {num1 / num2}")
+else:
+   print("Please enter one of the above mathematical operations")
+
+
+
+
+
+
+
