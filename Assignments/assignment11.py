a8f408ddaee121163a77a1d83c984f3ed5f158a9
Irene Iruoma <ireneiruoma@gmail.com>
2025-08-22 07:08:06 +0100

    updated assignment

---
+++ Assignments/assignment11.py
@@ -0,0 +1,16 @@
+"""
+this is a program that continues running until the user enters 'n'
+"""
+
+while True:  
+    Answer = input("Do you want to continue? (y/n): ")  
+
+    if Answer == "y":  
+        print("Fine, let's continue")  
+
+    elif Answer == "n":  
+        print(" Whyyy?? well you've come to the end of the program, Thank you!!")  
+        break  
+
+    else:  
+        print("Are you dumb?? Please input any of the above options")
